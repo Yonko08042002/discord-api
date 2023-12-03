7baa70718da58d873dda2b7b4b236adbe0fe8af0
Yonko08042002 <tin08042002@gmail.com>
2023-12-03 19:21:33 +0700

    post sign-in

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 
 export const config = {
   runtime: "edge",
@@ -7,17 +9,25 @@ export const config = {
 
 const app = new Hono().basePath("/api");
 
+app.use("*", logger());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.get("/", (c) => c.json({ message: "Hello Hono!" }));
 
 app
   .post("/sign-in", async (c) => {
     const { email, password } = await c.req.json();
-
-    if (email === "son.tran@gmail.com" && password === "!Enouvo123") {
+    // const body = await c.req.parseBody();
+    if (email === "tin.nguyen@gmail.com" && password === "!Enouvo123") {
       return c.json({ token: "124" });
     }
     return c.json({ error: "Invalid email or password" }, 401);
   })
   .post("/sign-up", (c) => c.json({ token: "124" }));
-  
+
 export default handle(app);
